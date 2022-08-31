b75fe476a2822d315f44cffb249c57a8b3c4eb31
Samuel Santana <samuelssc5874@gmail.com>
2022-08-31 12:00:00 -0300

    connect database

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
+import "reflect-metadata";
 import { AppDataSource } from "./data-source";
 import { User } from "./entity/User";
-import "reflect-metadata";
 
 AppDataSource.initialize()
   .then(async () => {
     console.log("Inserting a new user into the database...");
     const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
+    user.firstName = "Samuel";
+    user.lastName = "Santana";
+    user.age = 20;
     await AppDataSource.manager.save(user);
     console.log("Saved a new user with id: " + user.id);
 
