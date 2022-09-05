e3cb620032b47d2a0d24df9e009efe29a6816d96
Samuel Santana <samuelssc5874@gmail.com>
2022-09-05 16:47:54 -0300

    added conect database and created validation password

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { AppDataSource } from "./data-source";
 
 import { ApolloServer, gql } from "apollo-server";
+import { User } from "./entity/User";
 
 const typeDefs = gql`
   input UserInput {
@@ -32,20 +33,30 @@ const resolvers = {
     hello: () => "Hello Word!",
   },
   Mutation: {
-    createUser: (parent, args) => {
-      const data = {
-        id: 1,
-        name: args.data.name,
-        email: args.data.email,
-        birthDate: args.data.birthDate,
-      };
-      return data;
+    createUser: async (parent, args) => {
+      /^((?=\S*?[a-z,A-Z])(?=\S*?[0-9]).{6,})\S/.test(args.data.password) ||
+        console.log(
+          "A senha deve possuir ao menos 6 caracteres, com 1 letra e 1 numero"
+          // ver como lançar erro como resposta
+        );
+
+      // const validationEmail =
+
+      const user = new User();
+      user.name = args.data.name;
+      user.email = args.data.email;
+      user.birthDate = args.data.birthDate;
+      user.password = args.data.password;
+      await AppDataSource.manager.save(user);
+
+      return user;
     },
   },
 };
 
 const port = 4000;
 AppDataSource.initialize();
+
 const app = new ApolloServer({ typeDefs, resolvers });
 app
   .listen(port)
