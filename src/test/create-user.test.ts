1019a46fff964a17b718b2b3357a571bd7588bc9
Samuel Santana <samuelssc5874@gmail.com>
2022-09-16 13:57:37 -0300

    added tests in error on create-user

--- src/test/create-user.test.ts
+++ src/test/create-user.test.ts
@@ -14,17 +14,15 @@ describe("Mutation createUser", () => {
         }
       }
       `;
-
+  const urlDB = "http://localhost:4000/graphql";
   const USER_INPUT = {
-    name: "testando buscar os dados",
-    email: "SamuelTeste@gmail.com",
+    name: "Samuel Satana",
+    email: "SamuelTeste1@gmail.com",
     birthDate: "21/02/2002",
     password: "1234qwer",
   };
 
   it("should return createUser", async () => {
-    const urlDB = "http://localhost:4000/graphql";
-
     const response = await axios.post(urlDB, {
       variables: {
         data: USER_INPUT,
@@ -43,8 +41,8 @@ describe("Mutation createUser", () => {
       data: {
         createUser: {
           id: userCreated.id,
-          email: "SamuelTeste@gmail.com",
-          name: "testando buscar os dados",
+          email: "SamuelTeste1@gmail.com",
+          name: "Samuel Satana",
           birthDate: "21/02/2002",
         },
       },
@@ -55,7 +53,61 @@ describe("Mutation createUser", () => {
     expect(userCreated.email).to.be.deep.eq(USER_INPUT.email);
     expect(userCreated.birthDate).to.be.deep.eq(USER_INPUT.birthDate);
     expect(passwordCompare).to.be.deep.eq(true);
-    await dataSource.query(`DELETE FROM "user" WHERE id = ${userCreated.id}`);
+    await dataSource.query('TRUNCATE TABLE "user"');
   });
-  //Criar outros tests (its) para ver casos de erros
+
+  it("should return invalid password format", async () => {
+    const inputTestPassword = {
+      name: "Samuel Satana",
+      email: "SamuelTeste@gmail.com",
+      birthDate: "21/02/2002",
+      password: "123",
+    };
+
+    const response = await axios.post(urlDB, {
+      variables: {
+        data: inputTestPassword,
+      },
+      query: mutation,
+    });
+
+    const expectedResponse = {
+      message:
+        "A senha deve possuir ao menos 6 caracteres, com 1 letra e 1 numero",
+      code: 400,
+    };
+    expect(response.data.errors[0].message).to.be.deep.equal(
+      expectedResponse.message
+    );
+    expect(response.data.errors[0].extensions.exception.code).to.be.deep.equal(
+      expectedResponse.code
+    );
+  });
+
+  it("should return email already exist", async () => {
+    await axios.post(urlDB, {
+      variables: {
+        data: USER_INPUT,
+      },
+      query: mutation,
+    });
+    const response = await axios.post(urlDB, {
+      variables: {
+        data: USER_INPUT,
+      },
+      query: mutation,
+    });
+
+    const expectedResponse = {
+      message: "Este email já esta cadastrado",
+      code: 409,
+    };
+    expect(response.data.errors[0].message).to.be.deep.equal(
+      expectedResponse.message
+    );
+    expect(response.data.errors[0].extensions.exception.code).to.be.deep.equal(
+      expectedResponse.code
+    );
+    await dataSource.query('TRUNCATE TABLE "user"');
+  });
 });
