dd98fb27db4759ac0ca4117131c2222f6974b263
Samuel Santana <samuelssc5874@gmail.com>
2022-09-15 22:01:13 -0300

    adjusted test, added expects and clear database

--- src/test/create-user.test.ts
+++ src/test/create-user.test.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { expect } from "chai";
+import { dataSource } from "../data-source";
+import { User } from "../entity/User";
+import * as bcrypt from "bcrypt";
 
 describe("Mutation createUser", () => {
   const mutation = `mutation CreateUser($data: UserInput!) {
@@ -12,8 +15,8 @@ describe("Mutation createUser", () => {
       }
       `;
 
-  const VARIABLES = {
-    name: "Samuel Teste passando os dados",
+  const USER_INPUT = {
+    name: "testando buscar os dados",
     email: "SamuelTeste@gmail.com",
     birthDate: "21/02/2002",
     password: "1234qwer",
@@ -24,22 +27,34 @@ describe("Mutation createUser", () => {
 
     const response = await axios.post(urlDB, {
       variables: {
-        data: VARIABLES,
+        data: USER_INPUT,
       },
       query: mutation,
     });
+    const userCreated = await dataSource.findOneBy(User, {
+      email: USER_INPUT.email,
+    });
+    const passwordCompare = await bcrypt.compare(
+      USER_INPUT.password,
+      userCreated.password
+    );
 
     const expectedResponse = {
       data: {
         createUser: {
-          id: 2,
+          id: userCreated.id,
           email: "SamuelTeste@gmail.com",
-          name: "Samuel Teste passando os dados",
+          name: "testando buscar os dados",
           birthDate: "21/02/2002",
         },
       },
     };
     expect(response.status).to.equal(200);
     expect(response.data).to.be.deep.eq(expectedResponse);
+    expect(userCreated.name).to.be.deep.eq(USER_INPUT.name);
+    expect(userCreated.email).to.be.deep.eq(USER_INPUT.email);
+    expect(userCreated.birthDate).to.be.deep.eq(USER_INPUT.birthDate);
+    expect(passwordCompare).to.be.deep.eq(true);
+    await dataSource.query(`DELETE FROM "user" WHERE id = ${userCreated.id}`);
   });
 });
