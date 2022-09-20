77162bfc1ff6cd90dfe16fe2cfab08eda258d6b7
Samuel Santana <samuelssc5874@gmail.com>
2022-09-20 10:39:03 -0300

    added afterEach and renamed variables

--- src/test/create-user.test.ts
+++ src/test/create-user.test.ts
@@ -4,6 +4,10 @@ import { dataSource } from "../data-source";
 import { User } from "../entity/User";
 import * as bcrypt from "bcrypt";
 
+afterEach(async function () {
+  await dataSource.query('TRUNCATE TABLE "user"');
+});
+
 describe("Mutation createUser", () => {
   const mutation = `mutation CreateUser($data: UserInput!) {
         createUser(data: $data) {
@@ -15,7 +19,7 @@ describe("Mutation createUser", () => {
       }
       `;
   const urlDB = "http://localhost:4000/graphql";
-  const USER_INPUT = {
+  let input = {
     name: "Samuel Satana",
     email: "SamuelTeste1@gmail.com",
     birthDate: "21/02/2002",
@@ -25,22 +29,22 @@ describe("Mutation createUser", () => {
   it("should return createUser", async () => {
     const response = await axios.post(urlDB, {
       variables: {
-        data: USER_INPUT,
+        data: input,
       },
       query: mutation,
     });
-    const userCreated = await dataSource.findOneBy(User, {
-      email: USER_INPUT.email,
+    const userDb = await dataSource.findOneBy(User, {
+      email: input.email,
     });
     const passwordCompare = await bcrypt.compare(
-      USER_INPUT.password,
-      userCreated.password
+      input.password,
+      userDb.password
     );
 
     const expectedResponse = {
       data: {
         createUser: {
-          id: userCreated.id,
+          id: userDb.id,
           email: "SamuelTeste1@gmail.com",
           name: "Samuel Satana",
           birthDate: "21/02/2002",
@@ -49,24 +53,44 @@ describe("Mutation createUser", () => {
     };
     expect(response.status).to.equal(200);
     expect(response.data).to.be.deep.eq(expectedResponse);
-    expect(userCreated.name).to.be.deep.eq(USER_INPUT.name);
-    expect(userCreated.email).to.be.deep.eq(USER_INPUT.email);
-    expect(userCreated.birthDate).to.be.deep.eq(USER_INPUT.birthDate);
+    expect(userDb.name).to.be.deep.eq(input.name);
+    expect(userDb.email).to.be.deep.eq(input.email);
+    expect(userDb.birthDate).to.be.deep.eq(input.birthDate);
     expect(passwordCompare).to.be.deep.eq(true);
-    await dataSource.query('TRUNCATE TABLE "user"');
+  });
+
+  it("should return email already exist", async () => {
+    await axios.post(urlDB, {
+      variables: {
+        data: input,
+      },
+      query: mutation,
+    });
+    const response = await axios.post(urlDB, {
+      variables: {
+        data: input,
+      },
+      query: mutation,
+    });
+
+    const expectedResponse = {
+      message: "Este email já esta cadastrado",
+      code: 409,
+    };
+    expect(response.data.errors[0].message).to.be.deep.equal(
+      expectedResponse.message
+    );
+    expect(response.data.errors[0].extensions.exception.code).to.be.deep.equal(
+      expectedResponse.code
+    );
   });
 
   it("should return invalid password format", async () => {
-    const inputTestPassword = {
-      name: "Samuel Satana",
-      email: "SamuelTeste@gmail.com",
-      birthDate: "21/02/2002",
-      password: "123",
-    };
+    input.password = "123";
 
     const response = await axios.post(urlDB, {
       variables: {
-        data: inputTestPassword,
+        data: input,
       },
       query: mutation,
     });
@@ -82,32 +106,5 @@ describe("Mutation createUser", () => {
     expect(response.data.errors[0].extensions.exception.code).to.be.deep.equal(
       expectedResponse.code
     );
-  });
-
-  it("should return email already exist", async () => {
-    await axios.post(urlDB, {
-      variables: {
-        data: USER_INPUT,
-      },
-      query: mutation,
-    });
-    const response = await axios.post(urlDB, {
-      variables: {
-        data: USER_INPUT,
-      },
-      query: mutation,
-    });
-
-    const expectedResponse = {
-      message: "Este email já esta cadastrado",
-      code: 409,
-    };
-    expect(response.data.errors[0].message).to.be.deep.equal(
-      expectedResponse.message
-    );
-    expect(response.data.errors[0].extensions.exception.code).to.be.deep.equal(
-      expectedResponse.code
-    );
-    await dataSource.query('TRUNCATE TABLE "user"');
   });
 });
