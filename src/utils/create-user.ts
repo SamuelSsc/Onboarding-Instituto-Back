5929e6be8d68ed3ee575e6df894cfa1ecbd7aafc
Samuel Santana <samuelssc5874@gmail.com>
2022-09-28 10:31:03 -0300

    updated create-user function and user.test

--- src/utils/create-user.ts
+++ src/utils/create-user.ts
@@ -1,13 +1,25 @@
 import { dataSource } from "../data-source";
-import { User } from "../entity";
+import { Address, User } from "../entity";
 import * as bcrypt from "bcrypt";
+import { Index } from "typeorm";
 
 interface CreateUserProps {
   name?: string;
   email?: string;
   birthDate?: string;
   password?: string;
+  addressess?: AddressProps[];
 }
+
+export const defaultAdress = {
+  cep: "123456798",
+  city: "São Paulo",
+  state: "Sp",
+  complement: null,
+  street: "Luis Grassman",
+  streetNumber: 200,
+  neighborhood: "Jardim Mirante ZS",
+};
 
 export const defaultUser = {
   name: "Samuel Santana",
@@ -27,5 +39,40 @@ export async function CreateUser(props: CreateUserProps) {
   user.email = props.email ?? defaultUser.email;
   user.birthDate = props.birthDate ?? defaultUser.birthDate;
   user.password = passwordHashed;
+
+  const addressess = [];
+  props?.addressess?.map((addressItem) => {
+    const address = new Address();
+    (address.cep = addressItem.cep), (address.city = addressItem.city);
+    address.state = addressItem.state;
+    address.complement = addressItem.complement ?? null;
+    address.street = addressItem.street;
+    address.streetNumber = addressItem.streetNumber;
+    address.neighborhood = addressItem.neighborhood;
+
+    addressess.push(address);
+  });
+
+  const addressdefault = new Address();
+  addressdefault.cep = defaultAdress.cep;
+  addressdefault.city = defaultAdress.city;
+  addressdefault.state = defaultAdress.state;
+  addressdefault.complement = defaultAdress.complement;
+  addressdefault.street = defaultAdress.street;
+  addressdefault.streetNumber = defaultAdress.streetNumber;
+  addressdefault.neighborhood = defaultAdress.neighborhood;
+  addressess.push(addressdefault);
+
+  user.addresses = addressess;
   await dataSource.save(user);
+}
+
+interface AddressProps {
+  cep: string;
+  street: string;
+  streetNumber: number;
+  neighborhood: string;
+  city: string;
+  state: string;
+  complement?: string;
 }
